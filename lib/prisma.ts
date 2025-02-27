799ecb00c44afe1112e8626b9e6041806a1a0489
sebastien lato <sebastienlato@gmail.com>
2025-02-27 11:01:08 -0500

    Home Layout fix lint bug

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -5,9 +5,16 @@ const prismaClientSingleton = () => {
 };
 
 declare global {
+  // eslint-disable-next-line no-var
   var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
 }
 
-export const prisma = globalThis.prisma ?? prismaClientSingleton();
+type GlobalThisWithPrisma = typeof globalThis & {
+  prisma: ReturnType<typeof prismaClientSingleton> | undefined;
+};
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
+export const prisma =
+  (globalThis as GlobalThisWithPrisma).prisma ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== "production")
+  (globalThis as GlobalThisWithPrisma).prisma = prisma;
