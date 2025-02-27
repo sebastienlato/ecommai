ff5cef8d0c33d063f7ab59a8705dcd1948d1302a
sebastien lato <sebastienlato@gmail.com>
2025-02-27 11:26:33 -0500

    prisma errors fixed

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -15,7 +15,7 @@ export default async function Header() {
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         <div className="flex gap-6 md:gap-10">
           <Link href="/" className="flex items-center space-x-2">
-            <span className="inline-block font-bold">ECOMAI</span>
+            <span className="inline-block font-bold">ECOMMAI</span>
           </Link>
           <MainNav />
         </div>
